44de9aa95bb453924e6e0bc5d89b7481adc68841
Roberto Larivera <robylariv@gmail.com>
2023-03-15 07:59:42 +0100

    Creato migration update_column + Completato Bonus creazione ComicsTableSeeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Models
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+        foreach ($comics as $key => $item) {
+            # code...
+            $newComic =  new Comic;
+            $newComic->title =  $item['title'];
+            $newComic->description =  $item['description'];
+            $newComic->url =  $item['thumb'];
+            $newComic->price =  $item['price'];
+            $newComic->series =  $item['series'];
+            $newComic->sale_date =  $item['sale_date'];
+            $newComic->type =  $item['type'];
+            $newComic->save();
+        }
+    }
+}
